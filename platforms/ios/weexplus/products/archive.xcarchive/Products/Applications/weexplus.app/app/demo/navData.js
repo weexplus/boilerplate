e5943009c6c65586a1ae5ad39325c7ea5a80f524
郑江荣 <362675035@qq.com>
2018-10-26 14:11:50 +0800

    update

--- platforms/ios/weexplus/products/archive.xcarchive/Products/Applications/weexplus.app/app/demo/navData.js
+++
@@ -1,3 +0,0 @@
-// { "framework": "Vue"} 
-
-!function(e){function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}var t={};r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:n})},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},r.p="",r(r.s=39)}({39:function(e,r,t){"use strict";Object.defineProperty(r,"__esModule",{value:!0});var n=[{name:"导航",url:"module/navigator.js"},{name:"网络",url:"module/net.js"}];r.default=n}});
