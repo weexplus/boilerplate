f2771c987f144f853b13375b2d9bd1910ebad0aa
郑江荣 <362675035@qq.com>
2018-11-19 19:46:59 +0800

    fir

---
+++ .wxpScript/wxapi/WXEntryActivity.java
@@ -0,0 +1,13 @@
+package com.farwolf.wxpdemo.wxapi;
+
+import com.farwolf.wechat.WechatEntryActivity;
+
+/**
+ * Created by zhengjiangrong on 2018/5/31.
+ */
+
+public class WXEntryActivity extends WechatEntryActivity {
+
+
+
+}
---
+++ src/main/java/com/farwolf/wechat/WechatUtil.java
@@ -0,0 +1,19 @@
+package com.farwolf.wechat;
+
+/**
+ * Created by zhengjiangrong on 2018/6/1.
+ */
+
+public class WechatUtil {
+
+
+//    public static void init()
+//    {
+//
+//        try {
+//            WXSDKEngine.registerModule("wechat",WechatModule.class);
+//        } catch (WXException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
