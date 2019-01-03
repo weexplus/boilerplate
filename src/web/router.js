af951253b0c23f91ca1b9d0784e6252e1aed88e4
郑江荣 <362675035@qq.com>
2019-01-03 22:14:15 +0800

    web调整代码

--- src/web/router.js
+++ src/web/router.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 /*global Vue*/
 import Router from 'vue-router'
-import index from '../native/demo/index.vue'
-import nav from '../native/demo/nav.vue'
-import navigator from '../native/demo/module/navigator.vue'
-import drawer from '../native/demo/component/drawer.vue'
-import host from '../native/demo/component/host.vue'
+// import index from '../native/demo/index.vue'
 
 
 Vue.use(Router)
@@ -16,41 +12,13 @@ module.exports = new Router({
       path: '/',
      redirect:'/demo/index.js'
     },
-    {
-      path: '/demo/index.js',
-      name: 'index',
-      component: index,
-      meta:{
-        keepAlive:true
-      }
-
-    },
-    {
-      path: '/demo/nav.js',
-      name: 'nav',
-      component: nav,
-      meta:{
-        keepAlive:true
-      }
-    },
-    {
-      path: '/demo/module/navigator.js',
-      name: 'navigator',
-      component: navigator,
-      meta:{
-        keepAlive:true
-      }
-    },
-    {
-      path: '/demo/component/drawer.js',
-      name: 'drawer',
-      component: drawer,
-
-    },
-    {
-      path: '/demo/component/host.js',
-      name: 'host',
-      component: host
-    }
+    // {
+    //   path: '/demo/index.js',
+    //   name: 'index',
+    //   component: index,
+    //   meta:{
+    //     keepAlive:true
+    //   }
+    // }
   ]
 })
