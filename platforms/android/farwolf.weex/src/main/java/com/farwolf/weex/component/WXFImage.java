8f25ee4e04862ff40d2ca1072a84624b7466925d
郑江荣 <362675035@qq.com>
2019-01-18 21:59:19 +0800

    update

--- platforms/android/farwolf.weex/src/main/java/com/farwolf/weex/component/WXFImage.java
+++ platforms/android/farwolf.weex/src/main/java/com/farwolf/weex/component/WXFImage.java
@@ -1,6 +1,13 @@
 package com.farwolf.weex.component;
 
+import android.Manifest;
+import android.app.Activity;
+
+import com.farwolf.perssion.Perssion;
+import com.farwolf.perssion.PerssionCallback;
 import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.bridge.JSCallback;
 import com.taobao.weex.ui.action.BasicComponentData;
 import com.taobao.weex.ui.component.WXImage;
 import com.taobao.weex.ui.component.WXVContainer;
@@ -15,59 +22,16 @@ public class WXFImage extends WXImage {
     }
 
 
-//    public WXFImage(WXSDKInstance instance, WXDomObject node, WXVContainer parent) {
-//        super(instance, node, parent);
-//    }
+    @JSMethod(uiThread = false)
+    public void save(final JSCallback saveStatuCallback) {
+        Perssion.check((Activity) getContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE , new PerssionCallback() {
+            @Override
+            public void onGranted() {
 
-//    @WXComponentProp(name = Constants.Name.SRC)
-//    public void setSrc(String src) {
-
-
-
-//        String placeholder=null;
-//        WXAttr w= getDomObject().getAttrs();
-//        Set st=  w.entrySet();
-//        Iterator it= st.iterator();
-//        while(it.hasNext())
-//        {
-//           String kl= it.next()+"";
-//            System.out.println(kl);
-//        }
-//
-//        if(getDomObject().getAttrs().containsKey(Constants.Name.PLACEHOLDER)){
-//            placeholder= (String) getDomObject().getAttrs().get(Constants.Name.PLACEHOLDER);
-//        }else if(getDomObject().getAttrs().containsKey(Constants.Name.PLACE_HOLDER)){
-//            placeholder=(String)getDomObject().getAttrs().get(Constants.Name.PLACE_HOLDER);
-//        }
-
-//        if(placeholder!=null)
-//        {
-//            if(Weex.getBaseUrl(getInstance()).startsWith("http"))
-//            {
-//                placeholder= Weex.getRootUrl(placeholder,this.getInstance());
-////                placeholder= placeholder.replace(Weex.getBaseUrl(getInstance()),"app/");
-//            }
-//            else
-//            {
-//                placeholder= getRootUrl(placeholder,this.getInstance());
-//            }
-//            getDomObject().getAttrs().put(Constants.Name.PLACE_HOLDER,placeholder);
-//            getDomObject().getAttrs().put(Constants.Name.PLACEHOLDER,placeholder);
-//        }
-
-//        String p=getDomObject().getAttrs().get(Constants.Name.PLACE_HOLDER)+"";
-//        super.setSrc(src);
-//
-//
-//    }
-//
-//    @WXComponentProp(name = Constants.Name.PLACEHOLDER)
-//    public void setPlaceHolder(String placeHolder) {
-//
-//    }
-
-
-
+                WXFImage.super.save(saveStatuCallback);
+            }
+        });
+    }
 
 
 }
