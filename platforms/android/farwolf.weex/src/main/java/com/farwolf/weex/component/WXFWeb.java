102ceda88e8f54df5168eb7efdb6e65fc7f5a4e1
郑江荣 <362675035@qq.com>
2019-04-21 00:13:19 +0800

    update

--- platforms/android/farwolf.weex/src/main/java/com/farwolf/weex/component/WXFWeb.java
+++ platforms/android/farwolf.weex/src/main/java/com/farwolf/weex/component/WXFWeb.java
@@ -1,7 +1,12 @@
 package com.farwolf.weex.component;
 
+import android.Manifest;
+import android.app.Activity;
 import android.webkit.WebView;
 
+import com.farwolf.perssion.Perssion;
+import com.farwolf.perssion.PerssionCallback;
+import com.farwolf.util.SDCard;
 import com.farwolf.weex.core.local.Local;
 import com.farwolf.weex.util.Weex;
 import com.taobao.weex.WXSDKInstance;
@@ -30,10 +35,12 @@ public class WXFWeb extends WXWeb {
     public void setUrl(String url) {
         url= Weex.getRelativeUrl(url,this.mInstance);
         super.setUrl(url);
+
     }
 
     protected void createWebView(){
-        mWebView = new WXWebView(getContext(), "androidwebdata://");
+//        mWebView = new WXWebView(getContext(), "androidwebdata://");
+        mWebView = new WXWebView(getContext(), "/storage/emulated/0/Android/data/com.tark.rose/files/Caches/app/file/kline");
     }
 
 
@@ -42,24 +49,48 @@ public class WXFWeb extends WXWeb {
         WebView web=(WebView)mWebView.getView();
         if(web!=null)
             web.setHorizontalScrollBarEnabled(show);
+
     }
 
 
 
     @Override
-    public void loadUrl(String url) {
-        if(url.startsWith("http")){
-            super.loadUrl(url);
-        }else{
-            String param="";
-            String tempurl=url;
-            if(url.contains("?")){
-                param=  url.split("\\?")[1];
-                tempurl=url.split("\\?")[0];
+    public void loadUrl(final String url) {
+
+        Perssion.check((Activity) mInstance.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE,new PerssionCallback(){
+
+
+            @Override
+            public void onGranted() {
+
+                if(url.startsWith("http")){
+                    WXFWeb.super.loadUrl(url);
+                }else{
+                    String param="";
+                    String tempurl=url;
+                    if(url.contains("?")){
+                        param=  url.split("\\?")[1];
+                        tempurl=url.split("\\?")[0];
+                    }
+                    String s=Local.getString(mInstance.getContext(),tempurl);
+                    WXWebView web=(WXWebView)mWebView;
+
+                    web.getWebView().getSettings().setJavaScriptEnabled(true);
+                    web.getWebView().getSettings().setAllowFileAccessFromFileURLs(true);
+                    web.getWebView().getSettings().setAllowFileAccess(true);
+                    web.getWebView().getSettings().setAllowContentAccess(true);
+//            String base=  mInstance.getContext().getExternalFilesDir("Caches")
+//            web.getWebView().loadDataWithBaseURL("androidwebdata://?"+param,s,"text/html","utf-8","");
+                    String base="file://"+ SDCard.getBasePath(mInstance.getContext())+"/"+url+"?"+param;
+                    web.getWebView().loadDataWithBaseURL(base,s,"text/html","utf-8","");
+//            web.getWebView().loadDataWithBaseURL("file:///android_asset/app/file/kline",s,"text/html","utf-8","");
+                }
+
+
             }
-            String s=Local.getString(mInstance.getContext(),tempurl);
-            WXWebView web=(WXWebView)mWebView;
-            web.getWebView().loadDataWithBaseURL("androidwebdata://?"+param,s,"text/html","utf-8","");
-        }
+        });
+
+
+
     }
 }
