74f1b25d4d3e194aabc431f7e2b4a14a0c529188
郑江荣 <362675035@qq.com>
2019-05-29 11:41:46 +0800

    修复首页present跳转引起的bug

--- src/native/entry.vue
+++ src/native/entry.vue
@@ -3,21 +3,31 @@
     </div>
 </template>
 <script>
-  export default{
-    props: {},
-    data () {
-      return {}
-    },
-    methods: {
-      onLoad(){
-        let navigator=weex.requireModule('navigator')
-        navigator.presentFull({url:'root:index.js',animated:false})
-      }
-    },
-    created () {
+    export default{
+        props: {},
+        data () {
+            return {}
+        },
+        methods: {
+            onLoad(){
+                let url = 'root:index.js'
+                this.goToMain(url)
+            },
+            goToMain(url){
+                let navigator = weex.requireModule('navigator')
+                this.log(weex.config.env.platform)
+                if (weex.config.env.platform != 'iOS') {
+                    //注意android不要用presentFull
+                    navigator.pushFull({url: url, animated: false})
+                } else {
+                    navigator.presentFull({url: url, animated: false})
+                }
+            }
+        },
+        created () {
 
+        }
     }
-  }
 </script>
 <style scoped>
 </style>
